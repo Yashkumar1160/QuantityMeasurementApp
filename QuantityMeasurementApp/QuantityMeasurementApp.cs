b63ca52c2d920304246ac95bab586d64610088a2
Yashkumar1160 <yk4442144@gmail.com>
2026-02-21 15:12:49 +0530

    [Yash Kumar] Update Main program to use QuantityLength instead of Feet/Inches classes (UC-3)

---
+++ QuantityMeasurementApp/QuantityMeasurementApp.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantityMeasurementApp
+{
+    public class QuantityMeasurementApp
+    {
+        public static void Main(string[] args)
+        {
+            DemonstrateLengthEquality();
+        }
+
+        public static void DemonstrateLengthEquality()
+        {
+            // Take first value
+            Console.WriteLine("Enter First Value: ");
+            double firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter First Unit (Feet/Inch): ");
+            LengthUnit firstUnit = (LengthUnit)Enum.Parse(typeof(LengthUnit), Console.ReadLine(), true);
+
+            // Take second value
+            Console.WriteLine("Enter Second Value: ");
+            double secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Unit (Feet/Inch): ");
+            LengthUnit secondUnit = (LengthUnit)Enum.Parse(typeof(LengthUnit), Console.ReadLine(), true);
+
+            // Create QuantityLength objects
+            QuantityLength first = new QuantityLength(firstValue, firstUnit);
+            QuantityLength second = new QuantityLength(secondValue, secondUnit);
+
+            // Compare
+            Console.WriteLine(first.Equals(second)
+                ? "Both Values Are Equal"
+                : "Both Values Are Different");
+        }
+    }
+}
