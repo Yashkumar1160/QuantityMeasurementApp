c278e05586560587d19bec11d03c37f10629cb53
Yashkumar1160 <yk4442144@gmail.com>
2026-03-14 16:24:05 +0530

    [Yash Kumar] Add Application Layer with Controller Menu and Entry Point (UC-15)

---
+++ QuantityMeasurementApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace QuantityMeasurementApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             QuantityMeasurementApp.GetInstance().Start();
+        }
+    }
+}
