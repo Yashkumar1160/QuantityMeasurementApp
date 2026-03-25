e3a3d2411ad1001b322cf52951491eaee34dce3b
Yashkumar1160 <yk4442144@gmail.com>
2026-03-25 22:00:49 +0530

    [Yash Kumar] Add UC-17 Business Layer with IMeasurable interface, Implementations and Generic Quantity class (UC-17)

---
+++ QuantityMeasurementAppBusiness/Exceptions/DatabaseException.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace QuantityMeasurementAppBusiness.Exceptions
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException(string message) : base(message)
+        {
+        }
+
+        public DatabaseException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
